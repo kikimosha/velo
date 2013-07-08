ede157f65e1f88b7f3530a6bc396ce450dd1a2ff
Kaifat <tosha_kot@ua.fm>
2013-07-08 23:08:36 +0300

    admin parol

--- www/protected/modules/admin/controllers/DefaultController.php
+++ www/protected/modules/admin/controllers/DefaultController.php
@@ -1,22 +1,84 @@
 <?php
 
-class DefaultController extends Controller
+class DefaultController extends CController
 {
+    public $layout='/layouts/main';
 
-	public function actionIndex()
-	{
-		$this->render('index');
-	}
-
-    public function actionSection()
+    public function getPageTitle()
     {
-        $model = new Sections();
-        if(isset($_GET['id'])) {
+        if($this->action->id==='index')
+            return 'Admin Panel - Main page';
+        else
+            return 'Admin Panel - '.ucfirst($this->action->id).' Actionr';
+    }
 
-            $id = (int)$_GET['id'];
-            $sectionInfo = $model->findByPk($id);
-            $trips = Trips::model()->findAll("section_id = ". $id);
-            $this->render('section', array('sectionInfo' => $sectionInfo, 'trips' => $trips));
+    public function actionIndex()
+    {
+        $this->render('index');
+    }
+
+    public function actionError()
+    {
+        if($error=Yii::app()->errorHandler->error)
+        {
+            if(Yii::app()->request->isAjaxRequest)
+                echo $error['message'];
+            else
+                $this->render('error', $error);
         }
     }
+
+    /**
+     * Displays the login page
+     */
+    public function actionLogin()
+    {
+        $model=Yii::createComponent('admin.models.LoginForm');
+
+        // collect user input data
+        if(isset($_POST['LoginForm']))
+        {
+            $model->attributes=$_POST['LoginForm'];
+            // validate user input and redirect to the previous page if valid
+            if($model->validate() && $model->login())
+                $this->redirect(Yii::app()->createUrl('admin/default/index'));
+        }
+        // display the login form
+        $this->render('login',array('model'=>$model));
+    }
+
+    /**
+     * Logs out the current user and redirect to homepage.
+     */
+    public function actionLogout()
+    {
+        Yii::app()->user->logout(false);
+        $this->redirect(Yii::app()->createUrl('admin/default/index'));
+    }
+}
+
+
+//class DefaultController extends Controller
+//{
+//
+//	public function actionIndex()
+//	{
+//		$this->render('index');
+//	}
+//
+//    public function actionSection()
+//    {
+//        $model = new Sections();
+//        if(isset($_GET['id'])) {
+//
+//            $id = (int)$_GET['id'];
+//            $sectionInfo = $model->findByPk($id);
+//            $trips = Trips::model()->findAll("section_id = ". $id);
+//            $this->render('section', array('sectionInfo' => $sectionInfo, 'trips' => $trips));
+//        }
+//    }
+//}
+
+
+
 }
